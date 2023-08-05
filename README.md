8ac4b724f305d7ee64897cac30b8c768afdecd60
Camilo Velasquez <camilox168@outlook.com>
2023-08-05 09:56:53 -0500

    startingRepository

---
+++ README.md
@@ -0,0 +1,4 @@
+## AdministradorDeCitasVeterinariaActualizadoREACT
+
+Administra las citas de una Veterinaria, permitiendo editar o eliminar la cita agendada.
+(Este proyecto se hizo solo con Js) ... actualizado a react
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.jsx"], /* De forma recursiva se le indica a tailwind donde encontrara clases de tailwind */
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
